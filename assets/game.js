b0afa4eb932df5ad733bcf58481d7f376a585157
That_Guy <eghove@gmail.com>
2018-09-30 07:56:20 -0500

    built a function that will assign random values to the botton variables. started playing with jquery without much success

--- assets/game.js
+++ assets/game.js
@@ -5,6 +5,12 @@ let winsTotal = 0;
 //creating lossesTotal variable
 let lossesTotal= 0;
 
+//creating variables for button values
+let button1 = 0;
+let button2 = 0;
+let button3 = 0;
+let button4 = 0;
+
 //======DECLARING GLOBAL FUNCTIONS======================
 
 //function that generates a random number between a min and max
@@ -19,16 +25,44 @@ function setGoalScore() {
     goalScore=getRandomInt(19,121);
 }
 
+let userScore=0;
+
+//function that adds button values to userScore; this doesn't work correctly
+function scoreAdder(x) {
+    userScore = userScore + x;
+    console.log(userScore);
+}
+
+//function the sets the value of the buttons
+function setButtons() {
+    button1=getRandomInt(1,13);
+    console.log("Button 1:" + button1); //for debugging purposes
+    button2=getRandomInt(1,13);
+    console.log("Button 2:" + button2); //for debugging purposes
+    button3=getRandomInt(1,13);
+    console.log("Button 3:" + button3); //for debugging purposes
+    button4=getRandomInt(1,13);
+    console.log("Button 4:" + button4); //for debugging purposes
+}
+
+
+
 
 //==========PLAYING AROUND=====================================
 //call setGoalScore
 setGoalScore();
 
+//call setButtons
+setButtons();
+
 //printing the goalScore to the screen
 document.getElementById("computerNumber").innerHTML = goalScore; 
+
+
 
 //prints winsTotal
 document.getElementById("totalWins").innerHTML = "Wins: " + winsTotal;
 
 //prints lossesTotal
 document.getElementById("totalLosses").innerHTML = "Losses: " + lossesTotal;
+
